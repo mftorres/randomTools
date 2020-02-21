4721659157a2bb70d76f5b907d555357e94e9cfa
MFTorresJ <mftorres27@gmail.com>
2020-02-21 18:42:10 +0100

    Create file
    Creates a file with target_contigs extracted per loci, iterating through the 'extracted_target_contigs_all_samples.fasta' file generated by secapr (https://github.com/AntonelliLab/seqcap_processor)

---
+++ create_targetfiles.py
@@ -0,0 +1,26 @@
+import re
+import os
+import glob
+import sys
+import time
+
+print(os.getcwd())
+path=os.getcwd()
+file='extracted_target_contigs_all_samples.fasta'
+
+Loci=[]
+Sample=[]
+Sequence=[]
+for line in open(os.path.join(path,'%s'%(file)),'r'):
+    # header
+    if '>' in line:
+        tempsearch=re.search('>([0-9]+)_(.+)\s.+',line)
+        Loci.append(tempsearch.group(1) if tempsearch else None)
+        Sample.append(tempsearch.group(2) if tempsearch else None)
+    else:
+        Sequence.append(line)
+for i in range(0,len(Loci),1):
+    file_fasta=open(os.path.join(path,'Loci_%s_extracted.fasta'%(Loci[i])),'a')
+    file_fasta.write('>%s\n%s'%(Sample[i],Sequence[i]))
+    file_fasta.close()
+print('Done')
